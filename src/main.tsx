c6bf327ad2089dcefd961e5e91b9d377b835bea2
bcline01 <brookecline.bc@gmail.com>
2024-12-20 23:17:10 -0500

    initial install

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import 'owl.carousel/dist/assets/owl.carousel.css'; {/* this line */}
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
+const rootElement = document.getElementById('root') as HTMLElement;
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
